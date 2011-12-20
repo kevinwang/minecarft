34ac3d7dc134931d9600fa2469eca833389d787e
Kevin Wang <kevin@Canopus.stuy.edu>
2011-12-20 10:15:44 -0500

    Created Display and drew a quad.

--- Minecarft/src/minecarft/Minecarft.java
+++ Minecarft/src/minecarft/Minecarft.java
@@ -5,17 +5,53 @@
 
 package minecarft;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.*;
+import static org.lwjgl.opengl.GL11.*;
+
 /**
  *
  * @author Kevin Wang
  */
 public class Minecarft {
+    
+    public void start() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, 800, 600, 0, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        
+        while (!Display.isCloseRequested()) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            
+            glColor3f(0.5f, 0.5f, 1.0f);
+            
+            glBegin(GL_QUADS);
+            glVertex2f(100,100);
+            glVertex2f(100 + 200, 100);
+            glVertex2f(100 + 200, 100 + 200);
+            glVertex2f(100, 100 + 200);
+            glEnd();
+            
+            Display.update();
+        }
+        Display.destroy();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Minecarft minecarft = new Minecarft();
+        minecarft.start();
     }
 
 }
