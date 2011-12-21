8496329e279ae09faa892e6c5b95dd99782149f2
Kevin Wang <born2run5000@yahoo.com>
2011-12-20 23:35:20 -0500

    Somehow got 3D to work!

---
+++ Minecarft/src/minecarft/CameraController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minecarft;
+
+import org.lwjgl.util.vector.Vector3f;
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ *
+ * @author kevin
+ */
+public class CameraController {
+    private Vector3f position = null;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    
+    public CameraController(float x, float y, float z) {
+        position = new Vector3f(x, y, z);
+    }
+    
+    public void yaw(float amount) {
+        yaw += amount;
+    }
+    
+    public void pitch(float amount) {
+        pitch += amount;
+    }
+    
+    public void walkForward(float distance) {
+        position.x -= distance * (float) Math.sin(Math.toRadians(yaw));
+        position.z += distance * (float) Math.cos(Math.toRadians(yaw));
+    }
+    
+    public void walkBackwards(float distance) {
+        position.x += distance * (float) Math.sin(Math.toRadians(yaw));
+        position.z -= distance * (float) Math.cos(Math.toRadians(yaw));
+    }
+    
+    public void strafeLeft(float distance) {
+        position.x -= distance * (float) Math.sin(Math.toRadians(yaw - 90));
+        position.z += distance * (float) Math.cos(Math.toRadians(yaw - 90));
+    }
+    
+    public void strafeRight(float distance) {
+        position.x -= distance * (float) Math.sin(Math.toRadians(yaw + 90));
+        position.z += distance * (float) Math.cos(Math.toRadians(yaw + 90));
+    }
+    
+    public void lookThrough() {
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        glTranslatef(position.x, position.y, position.z);
+    }
+}
--- Minecarft/src/minecarft/Minecarft.java
+++ Minecarft/src/minecarft/Minecarft.java
@@ -2,10 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package minecarft;
 
+import org.lwjgl.util.glu.GLU;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -15,7 +17,7 @@ import static org.lwjgl.opengl.GL11.*;
  * @author Kevin Wang
  */
 public class Minecarft {
-    
+
     public void start() {
         try {
             Display.setDisplayMode(new DisplayMode(800, 600));
@@ -25,38 +27,113 @@ public class Minecarft {
             e.printStackTrace();
             System.exit(0);
         }
-        
+
+        glViewport(0, 0, 800, 600);
+
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, 800, 600, 0, 1, -1);
+        GLU.gluPerspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
         glMatrixMode(GL_MODELVIEW);
-        
-        while (!Display.isCloseRequested()) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            
-            glColor3f(0.5f, 0.5f, 1.0f);
-            
-            glBegin(GL_QUADS);
-            glVertex2f(100,100);
-            glVertex2f(100 + 200, 100);
-            glVertex2f(100 + 200, 100 + 200);
-            glVertex2f(100, 100 + 200);
-            glEnd();
-            
-            glColor3f(1, 1, 1);
-            
-            glBegin(GL_QUADS);
-            int x = Mouse.getX();
-            int y = 600 - Mouse.getY();
-            glVertex2f(x - 100, y - 100);
-            glVertex2f(x + 100, y - 100);
-            glVertex2f(x + 100, y + 100);
-            glVertex2f(x - 100, y + 100);
-            glEnd();
-            
-            Display.update();
-        }
+
+        glShadeModel(GL_SMOOTH);						// Enable Smooth Shading
+        glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
+        glClearDepth(1.0f);								// Depth Buffer Setup
+        glEnable(GL_DEPTH_TEST);						// Enables Depth Testing
+        glDepthFunc(GL_LEQUAL);						// The Type Of Depth Testing To Do
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+
+        runLoop();
+
         Display.destroy();
+    }
+
+    public void runLoop() {
+        CameraController camera = new CameraController(0.0f, 0.0f, -7.0f);
+
+        float dx = 0.0f;
+        float dy = 0.0f;
+        float dt = 0.0f;
+        long lastTime = 0;
+        long time = 0;
+
+        float mouseSensitivity = 0.05f;
+        float movementSpeed = 10.0f;
+
+        Mouse.setGrabbed(true);
+
+        while (!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            time = Sys.getTime();
+            dt = (time - lastTime) / 1000.0f;
+            lastTime = time;
+
+            // Control camera yaw/pitch with mouse
+            dx = Mouse.getDX();
+            dy = -Mouse.getDY();
+            camera.yaw(dx * mouseSensitivity);
+            camera.pitch(dy * mouseSensitivity);
+
+            // Control camera position with keyboard
+            if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+                camera.walkForward(movementSpeed * dt);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+                camera.walkBackwards(movementSpeed * dt);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+                camera.strafeLeft(movementSpeed * dt);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+                camera.strafeRight(movementSpeed * dt);
+            }
+
+            // Begin drawing
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            drawCube(0, 0, 0);
+            
+            glLoadIdentity();
+            
+            camera.lookThrough();
+
+            Display.update();
+            Display.sync(60);
+        }
+    }
+
+    // TODO: Implement params
+    public void drawCube(float x, float y, float z) {
+        glBegin(GL_QUADS);		// Draw The Cube Using quads
+        glColor3f(0.0f, 1.0f, 0.0f);	// Color Blue
+        glVertex3f(1.0f, 1.0f, -1.0f);	// Top Right Of The Quad (Top)
+        glVertex3f(-1.0f, 1.0f, -1.0f);	// Top Left Of The Quad (Top)
+        glVertex3f(-1.0f, 1.0f, 1.0f);	// Bottom Left Of The Quad (Top)
+        glVertex3f(1.0f, 1.0f, 1.0f);	// Bottom Right Of The Quad (Top)
+        glColor3f(1.0f, 0.5f, 0.0f);	// Color Orange
+        glVertex3f(1.0f, -1.0f, 1.0f);	// Top Right Of The Quad (Bottom)
+        glVertex3f(-1.0f, -1.0f, 1.0f);	// Top Left Of The Quad (Bottom)
+        glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Quad (Bottom)
+        glVertex3f(1.0f, -1.0f, -1.0f);	// Bottom Right Of The Quad (Bottom)
+        glColor3f(1.0f, 0.0f, 0.0f);	// Color Red	
+        glVertex3f(1.0f, 1.0f, 1.0f);	// Top Right Of The Quad (Front)
+        glVertex3f(-1.0f, 1.0f, 1.0f);	// Top Left Of The Quad (Front)
+        glVertex3f(-1.0f, -1.0f, 1.0f);	// Bottom Left Of The Quad (Front)
+        glVertex3f(1.0f, -1.0f, 1.0f);	// Bottom Right Of The Quad (Front)
+        glColor3f(1.0f, 1.0f, 0.0f);	// Color Yellow
+        glVertex3f(1.0f, -1.0f, -1.0f);	// Top Right Of The Quad (Back)
+        glVertex3f(-1.0f, -1.0f, -1.0f);	// Top Left Of The Quad (Back)
+        glVertex3f(-1.0f, 1.0f, -1.0f);	// Bottom Left Of The Quad (Back)
+        glVertex3f(1.0f, 1.0f, -1.0f);	// Bottom Right Of The Quad (Back)
+        glColor3f(0.0f, 0.0f, 1.0f);	// Color Blue
+        glVertex3f(-1.0f, 1.0f, 1.0f);	// Top Right Of The Quad (Left)
+        glVertex3f(-1.0f, 1.0f, -1.0f);	// Top Left Of The Quad (Left)
+        glVertex3f(-1.0f, -1.0f, -1.0f);	// Bottom Left Of The Quad (Left)
+        glVertex3f(-1.0f, -1.0f, 1.0f);	// Bottom Right Of The Quad (Left)
+        glColor3f(1.0f, 0.0f, 1.0f);	// Color Violet
+        glVertex3f(1.0f, 1.0f, -1.0f);	// Top Right Of The Quad (Right)
+        glVertex3f(1.0f, 1.0f, 1.0f);	// Top Left Of The Quad (Right)
+        glVertex3f(1.0f, -1.0f, 1.0f);	// Bottom Left Of The Quad (Right)
+        glVertex3f(1.0f, -1.0f, -1.0f);	// Bottom Right Of The Quad (Right)
+        glEnd();
     }
 
     /**
@@ -66,5 +143,4 @@ public class Minecarft {
         Minecarft minecarft = new Minecarft();
         minecarft.start();
     }
-
 }
