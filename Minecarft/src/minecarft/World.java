5c46d92375410b430b93f423e02274d820b8fa48
Kevin Wang <born2run5000@yahoo.com>
2011-12-21 17:28:31 -0500

    Set up world array in World.

--- Minecarft/src/minecarft/World.java
+++ Minecarft/src/minecarft/World.java
@@ -9,5 +9,17 @@ package minecarft;
  * @author kevin
  */
 public class World {
+    public static final int X = 16;
+    public static final int Y = 16;
+    public static final int Z = 16;
+    public int[][][] world;
     
+    public World() {
+        world = new int[X][Y][Z];
+        // Generate terrain and fill world array
+    }
+    
+    public int[][][] getWorld() {
+        return world;
+    }
 }
