a6bf4541245451e7eccf2f310ff996ed4a808d14
Kevin Wang <born2run5000@yahoo.com>
2011-12-25 00:09:33 -0500

    Camera now moves properly in the y direction (up/down) in noclip mode.

--- Minecarft/src/minecarft/CameraController.java
+++ Minecarft/src/minecarft/CameraController.java
@@ -15,6 +15,7 @@ public class CameraController {
     private Vector3f position = null;
     private float yaw = 0.0f;
     private float pitch = 0.0f;
+    private boolean noclip = true;
     
     public CameraController(float x, float y, float z) {
         position = new Vector3f(x, y, z);
@@ -31,13 +32,27 @@ public class CameraController {
     }
     
     public void walkForward(float distance) {
-        position.x -= distance * (float) Math.sin(Math.toRadians(yaw));
-        position.z += distance * (float) Math.cos(Math.toRadians(yaw));
+        float dx = distance * (float) Math.sin(Math.toRadians(yaw));
+        float dz = distance * (float) Math.cos(Math.toRadians(yaw));
+        if (noclip) {
+            dx *= (float) Math.cos(Math.toRadians(pitch));
+            dz *= (float) Math.cos(Math.toRadians(pitch));
+            position.y += distance * (float) Math.sin(Math.toRadians(pitch));
+        }
+        position.x -= dx;
+        position.z += dz;
     }
     
     public void walkBackwards(float distance) {
-        position.x += distance * (float) Math.sin(Math.toRadians(yaw));
-        position.z -= distance * (float) Math.cos(Math.toRadians(yaw));
+        float dx = distance * (float) Math.sin(Math.toRadians(yaw));
+        float dz = distance * (float) Math.cos(Math.toRadians(yaw));
+        if (noclip) {
+            dx *= (float) Math.cos(Math.toRadians(pitch));
+            dz *= (float) Math.cos(Math.toRadians(pitch));
+            position.y -= distance * (float) Math.sin(Math.toRadians(pitch));
+        }
+        position.x += dx;
+        position.z -= dz;
     }
     
     public void strafeLeft(float distance) {
@@ -54,6 +69,6 @@ public class CameraController {
         glRotatef(pitch, 1.0f, 0.0f, 0.0f);
         glRotatef(yaw, 0.0f, 1.0f, 0.0f);
         glTranslatef(position.x, position.y, position.z);
-        System.out.println("x = " + position.x + " y = " + position.y + " z = " + position.z);
+        System.out.println("x = " + position.x + " y = " + position.y + " z = " + position.z + " yaw = " + yaw + "pitch = " + pitch);
     }
 }
