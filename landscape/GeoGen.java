0ca4ad99ea56eb1e992f4ddfc9b5de8cf87f8481
Shan Shi <sshi@localhost.localdomain>
2011-12-25 11:29:55 -0500

    what's a happenin?

--- landscape/GeoGen.java
+++ landscape/GeoGen.java
@@ -1,5 +1,9 @@
 import java.util.*;
 public class GeoGen{
+    public final int LENGTH = 256;
+    public final int WIDTH = 256;
+    public final int HEIGHT= 256;
+    //LENGTH = WIDTH = HEIGHT = 256;
     public GeoGen(){
 	Random r = new Random();
 	/*int[][][] world;
@@ -7,8 +11,8 @@ public class GeoGen{
 	generate(r.nextInt());
 	*/
 	int[][] world = perlinNoise();
-	for(int i = 0; i < 32; i++){
-	    for(int j = 0; j<32; j++){
+	for(int i = 0; i < LENGTH; i++){
+	    for(int j = 0; j< WIDTH; j++){
 		System.out.print(world[i][j]);
 	    }
 	    System.out.println();
@@ -17,7 +21,7 @@ public class GeoGen{
     public GeoGen(int s){
 	Random r = new Random(s);
 	int[][][] world;
-	world = new int[32][32][256];
+	world = new int[LENGTH][WIDTH][HEIGHT];
 	generate(r.nextInt());
     }
     public void generate(int a){
@@ -57,11 +61,24 @@ public class GeoGen{
     public int[][] perlinNoise(){
 	Random r = new Random();
 	int [][] ret,tmp;
-	tmp = ret = new int[32][32];
-	for(int h = 4; h > 0; h--){
-	    for(int i = 0; i < 32; i+=2*h){
-		for(int j= 0; j < 32; j+=2*h){
-		    tmp[i][j] = r.nextInt();
+	tmp = ret = new int[LENGTH][WIDTH];
+	for(int h = 16; h > 0; h--){ //4 iterations
+	    for(int i = 0; i < LENGTH; i+=h){ //x axis
+		for(int j= 0; j < WIDTH; j+=h){ //y axis
+		    int m,n,o,p;
+		    m = i-h;
+		    n = j-h;
+		    o = i+h;
+		    p = j+h;
+		    if(m<0){m=0;}
+		    if(n<0){n=0;}
+		    if(o>LENGTH){o=LENGTH;}
+		    if(p>WIDTH){p=WIDTH;}
+		    for(int k = m; k < o; k++){
+			for(int l = n; l < p; l++){
+			    tmp[k][l] = r.nextInt((int)(h/2));
+			}
+		    }
 		}
 	    }
 	    for(int i = 0; i < 32; i++){
